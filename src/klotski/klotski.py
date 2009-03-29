17b3b9260dedd4568f8fd120b968cfc545cd5f5d
hannes <hannes@6e1bb5da-9869-4ca5-99f7-fc0589df2fb7>
2009-03-29 00:53:32 +0000

    git-svn-id: http://2e3.de/svn/public/klotski_solver/trunk@125 6e1bb5da-9869-4ca5-99f7-fc0589df2fb7

--- src/klotski/klotski.py
+++ src/klotski/klotski.py
@@ -2,6 +2,8 @@
 
 import sys, string
 
+from tools import curry
+
 M = 5
 N = 4
 
@@ -13,7 +15,28 @@ PUZZLE = (
 	(9, 0, 0, 10)
 )
 
+
 VISITED = {}
+
+class State(object):
+	def __init__(self, field):
+		self.field = field
+	
+	def __eq__(self, other):
+		return self.field == other.field
+	
+	def __ne__(self, other):
+		return not self.__eq__(other)
+	
+	def __hash__(self):
+		return self.field.__hash__()
+	
+	def is_solution(self):
+		return False
+	
+	def get_cell_content(self, cell):
+		m,n = cell
+		return self.field[m][n]
 
 def is_visited(state):
 	return state in VISITED
@@ -26,7 +49,7 @@ def get_neighbor_cells(cell):
 	m,n = cell
 	for rm in xrange(m-1, m+2):
 		for rn in xrange(n-1, n+2):
-			if rm >= 0 and rm < M and rn >= 0 and rn < N and (rm != m or rn != n):
+			if rm >= 0 and rm < M and rn >= 0 and rn < N and ((rm == m) ^ (rn == n)):
 				result.append((rm,rn))
 	return result
 
@@ -37,7 +60,8 @@ def get_succ(state):
 		for n,part in enumerate(row):
 			if part == 0:
 				clear.append((m, n))
-	print clear 
+				
+	
 	return result
 
 		
@@ -47,6 +71,9 @@ def print_state(state):
 		sys.stdout.write(str(row) + "\n")
 			
 if __name__ == '__main__':
+	s = State(PUZZLE)
+	print s.__hash__()
+	
 	VISITED[PUZZLE] = True
 	print_state(PUZZLE)
 	for state in get_succ(PUZZLE):
