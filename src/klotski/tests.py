e638f30af08e5aef4194928101e2d61142467e8c
Hannes Struss <x@hannesstruss.de>
2010-03-28 13:20:28 +0200

    assert that field remains the same

--- src/klotski/tests.py
+++ src/klotski/tests.py
@@ -5,15 +5,22 @@ from klotski import State
 
 class TestState(unittest.TestCase):
 	def test_init(self):
-		s = State((
+		trivial = (
 			(0, 1, 1, 0),
 			(0, 1, 1, 0),
 			(0, 0, 0, 0),
 			(0, 0, 0, 0),
 			(0, 0, 0, 0)
-		))
+		)
+		
+		s = State(trivial)
 		
 		self.assertEqual(len(s.blocks), 1)
+		self.assertEqual(s.field, trivial)
+		
+		
+		
+		
 		
 if __name__ == '__main__':
 	unittest.main()
