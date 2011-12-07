391346ea092513d840b28db8a5e4115b150417dc
Hannes Struss <x@hannesstruss.de>
2011-12-08 00:29:09 +0100

    reorganized testcase module, added testcase for get_neighbor_cells

--- src/rewrite/klotski_solver.py
+++ src/rewrite/klotski_solver.py
@@ -5,6 +5,11 @@ Created on Nov 21, 2010
 @author: hannes
 '''
 
+DIRECTIONS = ["u", "l", "r", "d"]
+# rows/cols
+M = 5
+N = 4
+
 class Solver(object):
 	def __init__(self, puzzle):
 		self.puzzle = puzzle
@@ -15,10 +20,31 @@ class Solver(object):
 class StateSuccessorFinder(object):
 	"""creates all possible subsequent states from a given initial state"""
 	
+	def get_neighbor_cells(self, cell):
+		fields = DIRECTIONS[:]
+		result = {}
+		m,n = cell
+		for rm in xrange(m-1, m+2):
+			for rn in xrange(n-1, n+2):
+				if ((rm == m) ^ (rn == n)):
+					field = fields[0]
+					fields = fields[1:]
+					if rm >= 0 and rm < M and rn >= 0 and rn < N:
+						result[field] = (rm,rn)
+					else:
+						result[field] = None
+		return result
+	
+	def get_movable_directions_of_cell(self, cell):
+		return set()
+	
 	def get_successors(self, state):
-		return []
+		return set()
 	
 class State(object):
+	"""A snapshot of a klotski board. Its field is a tuple of rows, which are tuples of cells.
+	   Thus, cells are written as (row, col)
+	"""
 	def __init__(self, field):
 		self.field = field
 		
--- src/rewrite/solver_tests.py
+++ src/rewrite/solver_tests.py
@@ -7,7 +7,60 @@ Created on Nov 21, 2010
 
 import unittest
 
-from klotski_solver import State, StateSuccessorFinder
+from klotski_solver import State, StateSuccessorFinder, M, N
+
+
+class TestHashCodes(unittest.TestCase):
+	def setUp(self):
+		pass
+	
+	def test_hash(self):
+		self.assertEqual(hash(States.state1), hash(States.state2))
+		self.assertNotEqual(hash(States.state1), hash(States.state2_succ1))
+		self.assertNotEqual(hash(States.state2), hash(States.state2_succ1))
+
+class TestStateEquality(unittest.TestCase):
+	def test_equality(self):
+		self.assertEqual(States.state1, States.state1) #identity
+		
+		self.assertEqual(States.state1, States.state2)
+		self.assertEqual(States.state2, States.state1)
+		
+	def test_inequality(self):
+		self.assertNotEqual(States.state0, States.state1)
+		self.assertNotEqual(States.state1, States.state2_succ1)
+
+class TestStateSuccessors(unittest.TestCase):
+	def setUp(self):
+		self.t = StateSuccessorFinder()
+		
+	def test_successors_count(self):
+		succs = self.t.get_successors(States.state1)
+		self.assertEqual(len(succs), 5)
+		
+	def test_successors_correctness(self):
+		succs = self.t.get_successors(States.state1)
+		self.assertEqual(
+			set([States.state1_succ1, States.state1_succ2, States.state1_succ3, 
+			     States.state1_succ4, States.state1_succ5]),
+			succs
+		)
+
+class TestGetNeighborCells(unittest.TestCase):		
+	def test_get_neighbor_cells(self):
+		t = StateSuccessorFinder()
+		
+		self.assertEqual(t.get_neighbor_cells((0, 0)),
+			{"u": None, "r": (0, 1), "d": (1, 0), "l": None})
+		
+		self.assertEqual(t.get_neighbor_cells((1, 0)),
+			{"u": (0, 0), "r": (1, 1), "d": (2, 0), "l": None})
+		
+		self.assertEqual(t.get_neighbor_cells((M - 1, N - 1)),
+			{"u": (M - 2, N - 1), "r": None, "d": None, "l": (M - 1, N - 2)})
+		
+		self.assertEqual(t.get_neighbor_cells((1, 2)),
+			{"u": (0, 2), "r": (1, 3), "d": (2, 2), "l": (1, 1)})
 
 class States(object):
 	state0 = State((
@@ -82,44 +135,5 @@ class States(object):
 		(0, 0, 0, 0),
 	))
 	
-	
-
-class TestHashCodes(unittest.TestCase):
-	def setUp(self):
-		pass
-	
-	def test_hash(self):
-		self.assertEqual(hash(States.state1), hash(States.state2))
-		self.assertNotEqual(hash(States.state1), hash(States.state2_succ1))
-		self.assertNotEqual(hash(States.state2), hash(States.state2_succ1))
-
-class TestStateEquality(unittest.TestCase):
-	def test_equality(self):
-		self.assertEqual(States.state1, States.state1) #identity
-		
-		self.assertEqual(States.state1, States.state2)
-		self.assertEqual(States.state2, States.state1)
-		
-	def test_inequality(self):
-		self.assertNotEqual(States.state0, States.state1)
-		self.assertNotEqual(States.state1, States.state2_succ1)
-
-class TestStateSuccessors(unittest.TestCase):
-	def setUp(self):
-		self.t = StateSuccessorFinder()
-		
-	def test_successors_count(self):
-		succs = self.t.get_successors(States.state1)
-		self.assertEqual(len(succs), 5)
-		
-	def test_successors_correctness(self):
-		succs = self.t.get_successors(States.state1)
-		self.assertEqual(
-			set([States.state1_succ1, States.state1_succ2, States.state1_succ3, 
-			     States.state1_succ4, States.state1_succ5]),
-			succs
-		)
-		
-		
 if __name__ == '__main__':
 	unittest.main()
